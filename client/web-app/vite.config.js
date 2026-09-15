23ab7286a40e4e66adaab2993648b766b60341f6
jezzif <jezzif12@gmail.com>
2026-09-16 09:17:14 +1000

    Added CSP header to vite config file for development

--- client/web-app/vite.config.js
+++ client/web-app/vite.config.js
@@ -5,7 +5,18 @@ import { expand } from "dotenv-expand";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({command}) => {
+  let scriptSrcPolicy = "'self'";
+  let styleSrcPolicy = "'self'";
+  let connectSrcPolicy = "'self'";
+
+  if (command === "serve") {
+    scriptSrcPolicy = "'self' 'unsafe-inline' https://maps.googleapis.com";
+    styleSrcPolicy = "'self' 'unsafe-inline' https://unpkg.com";
+    connectSrcPolicy = "'self' ws://localhost:3000 ws://127.0.0.1:3000 http://localhost:3000 http://localhost:8080 https://maps.googleapis.com";
+  }
+
+
   const env = {
     parsed: {
       // Load root .env
@@ -27,10 +38,25 @@ export default defineConfig(() => {
       processEnv[`import.meta.env.${key}`] = JSON.stringify(env.parsed[key]);
     }
   }
+
+  
   
   return {
     define: processEnv,
     plugins: [react()],
-    server: { port: 3000 },
+    server: {
+      port: 3000,
+      headers: {
+        "Content-Security-Policy": [
+          "default-src 'self';",
+          `script-src ${scriptSrcPolicy};`,
+          `script-src-elem ${scriptSrcPolicy};`,
+          `style-src ${styleSrcPolicy};`,
+          `connect-src ${connectSrcPolicy};`,
+          "img-src 'self' blob: https: data:;",
+          "worker-src 'self' blob:;"
+        ].join(' ')
+      },
+    },
   };
 })
